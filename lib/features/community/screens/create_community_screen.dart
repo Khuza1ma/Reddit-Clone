acb2f22c12c94cb42721381e0ed30069ca432f99
deepss1 <khuzaimanrko@gmail.com>
2023-12-09 13:10:33 +0530

    App +Completed UI for community screen

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/features/auth/widgets/widget_ext.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -9,8 +10,57 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _CommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final communityNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    communityNameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Community'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Community Name'),
+            10.verticalSpace,
+            TextField(
+              controller: communityNameController,
+              decoration: const InputDecoration(
+                hintText: 'r/Community_name',
+                filled: true,
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 21,
+            ),
+            30.verticalSpace,
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                minimumSize: const Size(
+                  double.infinity,
+                  50,
+                ),
+              ),
+              child: const Text(
+                'Create a Community',
+                style: TextStyle(fontSize: 17),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
